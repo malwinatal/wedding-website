68e95fa3533c643fdc07538deef26a14ae8e8c44
David Gago <davidopg@gmail.com>
2022-06-25 16:19:10 +0100

    closing sidebar when item pressed, closes #18

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,15 +8,20 @@ import { Sidenav } from 'materialize-css';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  private sidenav: Sidenav | undefined;
 
   constructor (@Inject(DOCUMENT) private document: Document) {
     document.addEventListener('DOMContentLoaded', () => {
       var elems = document.querySelectorAll('.sidenav');
-      Sidenav.init(elems);
+      this.sidenav = Sidenav.init(elems)[0];
     });
   }
 
   ngOnInit(): void {
+  }
+
+  clickedSidenav() {
+    (this.sidenav as Sidenav).close();
   }
 
 }
