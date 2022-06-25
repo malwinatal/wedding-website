447ebe61f783cc34eb67c0ffd6adc2fdd77906eb
Malwina Tałan <malwina.tal@gmail.com>
2022-06-25 12:56:35 +0100

    started invitation section

--- src/app/invitation/invitation.component.ts
+++ src/app/invitation/invitation.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvitationComponent implements OnInit {
 
+  public guest: string = "human";
   constructor() { }
 
   ngOnInit(): void {
