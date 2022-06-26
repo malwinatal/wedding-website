a2b2fd906213bb1c68d00dae4d6899e0dbd19382
David Gago <davidopg@gmail.com>
2022-06-26 23:08:29 +0100

    added enumerator for companions, closes #23

--- src/app/models/Account.ts
+++ src/app/models/Account.ts
@@ -1,9 +1,11 @@
+import { Companions } from './Companions';
+
 export class Account {
   constructor(
     public id: string,
     public invitationName: string,
     public code: string,
-    public companions: number,
+    public companions: Companions,
     public count: number
   ) {}
 }
