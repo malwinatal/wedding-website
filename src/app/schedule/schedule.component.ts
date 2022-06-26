1b5c26d101f842339c9dcd44ccac551f5c576e0e
Malwina Tałan <malwina.tal@gmail.com>
2022-06-26 22:33:06 +0100

    added basic timeline

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
+  public scheduleItems = [
+    {
+      "time": "15:00",
+      "name": "welcome",
+      "translation": "SCHEDULE.WELCOME"
+    },
+    {
+      "time": "15:30",
+      "name": "ceremony",
+      "translation": "SCHEDULE.CEREMONY"
+    },
+    {
+      "time": "16:00",
+      "name": "meal",
+      "translation": "SCHEDULE.MEAL"
+    },
+    {
+      "time": "20:00",
+      "name": "grill",
+      "translation": "SCHEDULE.GRILL"
+    },
+    {
+      "time": "23:00",
+      "name": "cake",
+      "translation": "SCHEDULE.CAKE"
+    },
+    {
+      "time": "1:00",
+      "name": "soup",
+      "translation": "SCHEDULE.SOUP"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
