4e1aefd41743edc8d74972c2a028ea2ffa347cfa
Malwina Tałan <malwina.tal@gmail.com>
2022-06-27 23:56:03 +0100

    added rsvp draft

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rsvp',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RsvpComponent implements OnInit {
 
-  constructor() { }
+  public guests = [{"name": "Davidek"}, {"name": "Davidinho"}, {"name": "Davdav"}];
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
 
   ngOnInit(): void {
+    var elems = document.querySelectorAll('.chips');
+    M.Chips.init(elems, {"placeholder": "Add tag",	"secondaryPlaceholder": "+tag"});
   }
 
 }
