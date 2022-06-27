3c016a31c0b50b7ad55afb31a82052c51a8e9792
David Gago <davidopg@gmail.com>
2022-06-27 23:04:45 +0100

    fixed navbar, set location section vertically aligned

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,23 +5,27 @@ import { Sidenav } from 'materialize-css';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  private sidenav: Sidenav | undefined;
+  private sidenav!: Sidenav;
 
-  constructor (@Inject(DOCUMENT) private document: Document) {
-    document.addEventListener('DOMContentLoaded', () => {
-      var elems = document.querySelectorAll('.sidenav');
-      this.sidenav = Sidenav.init(elems)[0];
-    });
-  }
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   ngOnInit(): void {
+    var elems = document.querySelectorAll('.sidenav');
+    this.sidenav = Sidenav.init(elems)[0];
   }
 
   clickedSidenav() {
-    (this.sidenav as Sidenav).close();
+    this.sidenav.close();
   }
 
+  toggleSidenav() {
+    if (this.sidenav.isOpen) {
+      this.sidenav.close();
+    } else {
+      this.sidenav.open();
+    }
+  }
 }
