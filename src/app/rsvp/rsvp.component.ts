693f54551888b98c9921d27b25775a6636a07338
Malwina Tałan <malwina.tal@gmail.com>
2022-06-28 19:31:10 +0100

    improved ui of the rsp cards, closes #26, closes #25

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -8,14 +8,14 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 export class RsvpComponent implements OnInit {
 
-  public guests = [{"name": "Davidek"}, {"name": "Davidinho"}, {"name": "Davdav"}];
+  public guests = [{"name": "Davidek"}, {"name": "David de Oliveira Pinto Gago"}, {"name": "Guest Guest Guest Guest"}];
 
   constructor(@Inject(DOCUMENT) private document: Document) {
   }
 
   ngOnInit(): void {
     var elems = document.querySelectorAll('.chips');
-    M.Chips.init(elems, {"placeholder": "Add tag",	"secondaryPlaceholder": "+tag"});
+    M.Chips.init(elems, {"placeholder": "Add tag", "secondaryPlaceholder": "+tag"});
   }
 
 }
