f0b58e45b93d39cb8597061b9bdcb42c44d02ffb
Malwina Tałan <malwina.tal@gmail.com>
2022-07-02 15:36:38 +0100

    forms work (without chips)

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,7 +1,6 @@
 import { DOCUMENT } from '@angular/common';
-import { ArrayType, ThisReceiver } from '@angular/compiler';
 import { Component, Inject, OnInit, AfterViewInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-rsvp',
@@ -10,37 +9,44 @@ import { FormBuilder } from '@angular/forms';
 })
 export class RsvpComponent implements OnInit, AfterViewInit {
 
-  public guests = [{"name": "Davidek"}, {"name": "David de Oliveira Pinto Gago"}, {"name": "Guest Guest Guest Guest"}];
+  public guests = [{"id": 1, "name": "Davidek"}, {"id": 2, "name": "David de Oliveira Pinto Gago"}, {"id": 3, "name": "Guest Guest Guest Guest"}];
 
-  guestForm = this.formBuilder.group({
-    id: [-1],
-    name: ["lollz"],
-    going: [true],
-    diet: ["meat"],
-    allergies: this.formBuilder.group({tag: ["lolz"]})
-  });
-
+  guestForms = this.formBuilder.array(this.createFormGroups());
+  
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private formBuilder: FormBuilder
-  ) {
+  ) {  
   }
 
   instances: M.Chips[] = [];
 
   ngAfterViewInit(): void {
     var elems = document.querySelectorAll('.chips');
-    this.instances = M.Chips.init(elems, {"placeholder": "Add tag", "secondaryPlaceholder": "+tag", "onChipAdd": (e, chip) => console.log(this.instances[0].chipsData)});
+    this.instances = M.Chips.init(elems, {"placeholder": "Add tag", "secondaryPlaceholder": "+tag", "onChipAdd": (e, chip) => this.updateChips(), "onChipDelete": (e, chip) => this.updateChips()});
   }
 
   ngOnInit(): void {
-    this.guestForm.valueChanges.subscribe((form => console.log(form)));
+    this.guestForms.valueChanges.subscribe((form => console.log(form)));
   }
 
-  addChip(): void {
-    var arr = this.instances[0].chipsData;
-    console.log(arr);
-    console.log(this.guestForm.value.allergies?.tag);
+  createFormGroups(): Array<FormGroup> {
+      return this.guests.map(guest => this.createFormGroup(guest));
+  }
+
+  createFormGroup(guest: any): FormGroup {
+    return this.formBuilder.group({
+      id: [guest.id],
+      name: [guest.name],
+      going: [],
+      diet: [""],
+      allergies: this.formBuilder.group({tag: []})
+    });
+
+  }
+
+  updateChips(): void {
+    this.instances.map(i => console.log(i.chipsData));
     //this.guestForm.patchValue({allergies: arr});
   }
 
