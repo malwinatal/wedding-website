c70577a24c06381d2e61fb23005c7e691ae754f8
Malwina Tałan <malwina.tal@gmail.com>
2022-07-06 21:10:39 +0100

    added companies section, closes #35

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
   styleUrls: ['./companies.component.scss']
 })
-export class CompaniesComponent implements OnInit {
+export class CompaniesComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  public companyItems = [
+    {
+      "name": "Malwida",
+      "image": "../../assets/images/beach_square.jpg",
+      "web": "https://www.magicflashphoto.com"
+    },
+    {
+      "name": "Magic Flash Photo",
+      "image": "../../assets/images/magicflash.png",
+      "web": "https://www.magicflashphoto.com"
+    },
+    {
+      "name": "Oczyszczalnia Miejsce",
+      "image": "../../assets/images/oczyszczalnia.png",
+      "web": "https://oczyszczalniamiejsce.pl"
+    }
+  ];
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    var elems = document.querySelectorAll('.tooltipped');
+    console.log(elems)
+    M.Tooltip.init(elems, {});
   }
 
 }
