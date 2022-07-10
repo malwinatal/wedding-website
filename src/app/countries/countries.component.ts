48c0b8e65560adf25a2a09b827e43167aa716ff1
David Gago <davidopg@gmail.com>
2022-07-10 18:59:25 +0100

    added carousel for each country's data, closes #41

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import { portugalData, polandData } from '../data/CountriesData';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
 })
-export class CountriesComponent implements OnInit {
+export class CountriesComponent implements OnInit, AfterViewInit {
+  @ViewChild('carousel') carousel!: ElementRef;
+  @ViewChild('carousel2') carousel2!: ElementRef;
 
-  constructor() { }
+  portugalData = portugalData;
+  polandData = polandData;
 
-  ngOnInit(): void {
+  constructor() {}
+  ngAfterViewInit(): void {
+    M.Carousel.init(this.carousel.nativeElement, {
+      indicators: true,
+      fullWidth: true,
+    });
+    M.Carousel.init(this.carousel2.nativeElement, {
+      indicators: true,
+      fullWidth: true,
+    });
   }
 
+  ngOnInit(): void {}
 }
---
+++ src/app/data/CountriesData.ts
@@ -0,0 +1,27 @@
+interface CountryData {
+  img: string;
+  title: string;
+  shortDescription: string;
+  description: string;
+  link: string | null;
+}
+
+export const polandData: CountryData[] = [
+  {
+    img: 'https://i.imgur.com/nxMBqb4.gif',
+    title: 'Laghing goose',
+    shortDescription: 'Some short description',
+    description: 'Some long description',
+    link: null,
+  },
+];
+
+export const portugalData: CountryData[] = [
+  {
+    img: 'https://i.imgur.com/nxMBqb4.gif',
+    title: 'Laghing goose',
+    shortDescription: 'Some short description',
+    description: 'Some long description',
+    link: null,
+  },
+];
