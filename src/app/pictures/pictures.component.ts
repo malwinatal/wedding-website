861fe9b529226c047efb638ce4c625451be2f972
Malwina Tałan <malwina.tal@gmail.com>
2022-07-10 18:49:11 +0100

    added pictures section, closes #42

--- src/app/pictures/pictures.component.ts
+++ src/app/pictures/pictures.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pictures',
   templateUrl: './pictures.component.html',
   styleUrls: ['./pictures.component.scss']
 })
-export class PicturesComponent implements OnInit {
+export class PicturesComponent implements OnInit, AfterViewInit {
+  @ViewChild('carouselPictures') carouselPictures!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const carouselInstance = M.Carousel.init(this.carouselPictures.nativeElement, {indicators: true});
+    this.autoplayCarousel(carouselInstance);
+  }
+
+  autoplayCarousel(carouselInstance: M.Carousel): void {
+    carouselInstance.next();
+    setTimeout(() => this.autoplayCarousel(carouselInstance), 3000);
   }
 
 }
