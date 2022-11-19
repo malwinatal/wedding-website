27a6c9d4a8f0f11f29ae1f5ba1107fa0d7948886
David Gago <davidopg@gmail.com>
2022-11-19 18:58:08 +0000

    Added deployment stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDtcPVyDywBH-SNubRlK1YH4IllAJAeRh8',
+    authDomain: 'wedding-app-e58d1.firebaseapp.com',
+    projectId: 'wedding-app-e58d1',
+    storageBucket: 'wedding-app-e58d1.appspot.com',
+    messagingSenderId: '778958557833',
+    appId: '1:778958557833:web:50b1310813fdac05036c58',
+  },
 };
