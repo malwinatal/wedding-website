0f4e4e39cf031c6e033223a40db55bfd5af6d587
David Gago <davidopg@gmail.com>
2022-11-19 14:22:39 +0000

    Fix invitation gender translations, closes #61

--- src/app/invitation/invitation.component.ts
+++ src/app/invitation/invitation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Account } from '../models/Account';
+import { Companions } from '../models/Companions';
 import { AccountService } from '../services/account.service';
 
 @Component({
@@ -10,13 +12,34 @@ import { AccountService } from '../services/account.service';
 export class InvitationComponent implements OnInit {
   userAccount: Account | null;
   public guest: string = 'human';
-  public companions: string = 'with your cats';
+  public who: string = 'you';
 
-  constructor(accountService: AccountService) {
+  constructor(
+    accountService: AccountService,
+    private translate: TranslateService
+  ) {
     this.userAccount = accountService.account;
   }
 
   ngOnInit(): void {
     this.guest = this.userAccount!.invitationName;
+
+    this.translate.onLangChange.subscribe(() => {
+      switch (this.userAccount?.companions) {
+        case Companions.SINGLE:
+          this.who = this.translate.instant('INVITATION.INVITE.WHO.SINGLE');
+          break;
+        case Companions.PLUSONE:
+          this.who = this.translate.instant('INVITATION.INVITE.WHO.PLURAL');
+          break;
+        case Companions.WITHCHILDREN:
+          this.who = this.translate.instant(
+            'INVITATION.INVITE.WHO.WITH_CHILDREN'
+          );
+          break;
+        default:
+          break;
+      }
+    });
   }
 }
